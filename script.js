9bc09274dcdbc2b6d772d872097c435011154589
lynn pepin <lynn@lynndotpy.dev>
2025-12-26 11:44:20 -0500

    those are some good posts

---
+++ script.js
@@ -0,0 +1,297 @@
+raw_posts = [
+  "no",
+  "dude. look how hard i can cry",
+  "I love you THIIIIIIIIIIIIS much!!! 🤏",
+  "have all those years of playing runescape paid off yet",
+  "charging up my joe biden tulpa",
+  "santa claus is a fascist",
+  "posting is all i have in life",
+  "'i hate the french', i say, whitely,",
+  "ghosts are literally real",
+  "immortality rings stuck on penis help",
+  "piss",
+  "as your therapist, i sympathize with you and i can understand why that is troubling. but on the other hand, that is some serious seinfeld shit",
+  "(animal crossing rover voice) Your skin is so lovely, what shade is that? Fascinating. And are you circumcised?",
+  "rejected with consolation 'plenty of fish in the sea'. dodged a bullet. you can't date fish idiot. maybe a dolphin? but even then its tenuous",
+  "you gotta hand it to chasers,",
+  "benjamin franklin would have loved \"MILF\"s as a concept. and also as a milfs",
+  "'cool quartz necklace, what properties does it have?' well i purchased it because of the 'craigslist jackoff necklace guy' but the most magical thing about it is its abundance on earth and the piezoelectric effect making it a fantastic clock in electric circuits. but mostly the jacking off",
+  "my wife and kids left me behind at applebees after i challengd the waiter to an arm wrestle and completely lost my shit. again",
+  "i ll start voting once they make a presidential medal of gaming",
+  "exciting new kickstarter combines the modularity of a sectional sofa with the utility of a toilet",
+  "google search free minecraft boobies no viruses",
+  "what kind of catheter does iron man use",
+  "coming to the disappointing conclusions. and so forth",
+  "goethe's faust is a wildly influential Bitch who did not know what colors were",
+  "my (f21) boyfriend (m38)'s algorithm keeps showing him ai videos of a baby being ripped apart by pit bulls. god i love him so much",
+  "why are you all surprised that i am smart and well read? is it my illiteracy",
+  "fetishizing korean men in a way that is a little feminist and very racist",
+  "to all the bots telling me 'i need psychological help' and 'i am full of shit'.. you sound like my next ex wife. dm me",
+  "making fun of kids is a human past time since time imemorial",
+  "can anyone recommend a good anime? no",
+  "i am some sort of delirious man bitch who is not even a man",
+  "'Finally, FINALLY, an ACTUAL FIGHT WITH BOWSER. No Advancing Boss of Doom, no Get Back Here Boss, no Tennis Boss, just RUN UP, and PUNCH HIM IN HIS FACE. FINALLY' -- ThePoltergust5000 commenting on 'Super Mario Odyssey: Bowser Final Boss Fight and Ending'",
+  "the down with cis bus skipped my fucking stop",
+  "give me 'the chat gpt haircut'. i am ready to die",
+  "tenga egg review: the desk ornament which will get you fired",
+  "if luigi's mansione tried to shoot me i would have been impervious on account of my general qualities and attributes",
+  "looking for: DICKs. status: TIGHT",
+  "THE BITCH OF MATH aced another test",
+  "pythagorean theorem? more like piss that gay ass precum.",
+  "say what you will about elon musk, he did the impossible: get people to use Mastodon",
+  "they're eating the nintendogs. they're eating the nintencats. and some, i assume, are good nintenpeople",
+  "relaxing after a long day with a movie that sounds cute and cottagecore, 'the silence of the lambs'",
+  "you're a beautiful woman. you must be so good at videogames",
+  "very disappointed by 'block chain'",
+  "holiday was good. ate lots of cheese. grandma dead",
+  "with the seven gender emeralds in tow i finally defeated judith butler",
+  "i am successful and usually sort of moist",
+  "i independently arrived to the same idea as Dr. Seuss' book 'The Pocket Book of Boners' bitch",
+  "juice stand guy fucked the mango in front of me",
+  "you let your toddler on the escalator all alone. but im the bad guy for going up the down escalator like a linebacker demolishing everything in my way?",
+  "bog standard family structures don't stand a chance against wildly incestuous ones",
+  "centrist taking a side in the debate between 'wiping' and 'you dont need to wipe' positions",
+  "how i make my very good posts (CW: mental illness, suicidal ideation, drug abuse, sleep paralysis, dissociative disorder, software opinions, childhood sexual abuse)",
+  "say what you will about elon musk, he did the impossible: get dril to sign up for Threads as the only person posting at all",
+  "i only hate the French because it lets me practice hatred for ethnic minorities in a manner Woke and becoming of of my Brand",
+  "babe. i'm as circumcised as they get",
+  "goethe shaking his fists and rolling in his grave as SJWs use green instead of yellow",
+  "teacup? more like tea gallon. <-- british people probably",
+  "\"sexism is when girls can't play baseball\", i say, tears rolling down my eyes. my lovely wife is allowed, but so bad. \"but my lovely wife is good enough to make up for me\" says my lovely wife (who is bad at baseball). \"we are baseball lesbians\"",
+  "i am sick and tired of seeing ryan reynolds. (i am roommates with ryan reynolds)",
+  "tic tacs but instead of sugar it is salt",
+  "jimmy neutron's full name: james 'isaac' 'boy genius' 'james' isaac biden",
+  "paid $400 for an IQ test. explained all the plotholes in Avengers: Infinity War. is 'inconclusive' good or bad",
+  "haunting and deeply beautiful whistling from a man in the grocery store. one of the most beautiful things i've ever heard. didn't compliment him because i don't want to encourage that kind of behavior",
+  "(turning 25) i've only got another 10 years of trick or treating left in me",
+  "time to destroy bit coin",
+  "straight pride flag two star review on amazon",
+  "Triple chain gate. Ripple dome. Massive gear. Multi-step orb. Dynamic gear. Quad tower impact edge. These are the tools of jacking off",
+  "nothing is cringe except for 'based', which is cringe, and nothing is based except for 'cringe', which is based",
+  "i am so happy i was so wrong <-- i am the bitch of prophecy",
+  "do you know the muffin man? *shakes you by your collar* where is the fucking muffin man *screams*",
+  "Self Suck your Way to 100% Success and 0% Maxed Out Credit Cards",
+  "Mars candy corporation clarifies 'Three Musketeers' not a reference to the 1844 French Historical novel. they're a firing squad who executed an innocent man",
+  "ok computer. generate ursula k leguin short story about femboys. 8000 words. hugo finalist 2039. new york times best seller. erotic thriller.",
+  "playing a sick guitar solo on the fron lawn with fireworks and everything. my children hate me and my wife is leaving me",
+  "liberals want you to believe in IMAGINARY and IRRATIONAL NUMBERS now. this is 'new math'",
+  "i am the true yaoi bara swordsman number 1",
+  "a good cuck is not just dominant, but also has all the skills of a gameshow host. this property is associative",
+  "Oh ho ho, what is this? As someone who loves both lemons AND parties, I feel confident this 'click' on the 'world wide web' will be fruitful and joyous.",
+  "'woah im tired' said yaoi bara swordsman number 1. 'let's have a snack and stop fighting'",
+  "I've come to the realization that being silly is low effort. There are numerous ways to be silly, but only one way to be serious. For that reason, I will not be being silly any more. Please respect my decision at this time",
+  "microsoft teams is such Dogshit cratering my Productivity that it is given the Anticapitalist Award of the Decade",
+  "feeling bad about the current joe biden stuff",
+  "choose ur gender: *one of two identical skeletons*",
+  "you can :3 if you want to. you can leave your :o behind. cause your friends don't ',;) and if they don't ,';) then they're no friends of mine",
+  "taking the world by storm: coffee enemas",
+  "i'm not just 'good at posting'. babe. im the 'postmaster general'",
+  "I was in a rubber suit and I had rubber nipples. I could have played Batman straight, but I made him gay. Is Batman gay? No, but I made him gay",
+  "you've merried your last xbox christmas",
+  "there's a one in a billion chance that every time you used a fleshlight there's a chance your dick will be ground up and put into a patty to grill",
+  "stakeholders are scared of the new 'halloween all year' direction we are taking",
+//"fucked elf dot tumblr dot com using all his holes to spread christmas cheer",
+  "romanticizing feet in a god-honoring way",
+  "Biden Claimed His Uncle Was Eaten by Cannibals. Military Records Say Otherwise",
+  "congratulations to the \"death to america\" crowd for winning the war on america",
+  "times are so tight that i am now only tipping 15% to my landlord",
+  "my religious beliefs is. that the halo games are real and going to happen",
+  "pokemon go. pokemon sleep. pokemon smile. you sultry bitches thought i'd say pokemon piss, cum, shit? what about pokemon sob. pokemon bleed. what about that",
+  "\"try not to cum challenge\". buddy. that's not a challenge. that's the story of my life",
+  "neither of them could hear the tiny, tinny screaming from the tomato. it is neither a fruit nor a vegetable. in yaoi bara swordsman world, fruits are alive",
+  "guinea pigs are the sillyiest little guys. except for me",
+  "chao garden from Sonic Adventure 2 Battle reminded me of my childhood. because of the neglect and physical abuse",
+  "silly liberal. god gave us only twenty-seven genders: female, alpha male, beta male, gamma male,",
+  "you're allowed to have a grimoire of curses, etc. even if your breasts are small",
+  "twitter dot com is the same thing as hitler youth and im not joking",
+  "(reviewing jack off device) every part has the purpose of obliteration, maceration, destruction, etc.",
+  "i, for one, am excited for Obama to take me FEMA camping",
+  "i asked chatgpt for the 100 days of christmas. what happened next made me shake, shit, cum, and so forth",
+  "Peeled grapes? Wet spaghetti? Who cares? Give your family a Halloween to remember with Horsebrook Farms AUTHENTIC 'Entrails and Viscera' Halloween experience kit!",
+  "i'm not 'jacking off' im 'cultivating pleasure'. and babe. you're a scarecrow",
+  "just three words for you: windows 12 is here",
+  "platonic ideal homosexual",
+  "i step into my jesus slippers (to remind myself that Hᴇ is always beneath me) and i put on my satan cap (to remind myself that satan is my lord)",
+  "i can say that because im gay",
+  "annointed the Commune Crown and reluctantly must embrace the title of Landlord",
+  "ladies. im gay",
+  "god. it hurts",
+  "i believe in myself, despite my complete and total lack of a foot fetish",
+  "this account has been brought to you by Mariah Menunounos",
+  "the woke left has cancelled Dr Seuss for 'the pocket book of boners' one too many times",
+  "driving is like a nintendo Gamecube game. 'STEEL PLATES IN ROAD' yeah 'NEW TRAFFIC PATTERN' i gotta buy this",
+  "if you ask me about the reward program i will kill myself right here. hey do you have apple pay",
+  "man who secretly wants to be a cuck. but everyone he brings to fuck his wife just wants to watch. that is my tragedy",
+  "just got my bechdel test back. it's positive",
+  "incel: wants to fuck, doesn't. volcel: doesn't want to fuck, doesn't. me? well. let's just say. :(",
+  "import numpy as SHIT. import sympy as GARBAGE",
+  "https://en.wikipedia.org/wiki/Evil_number",
+  "THANK YOU feminism for not killing games",
+  "is my childhood trauma really that funny to you",
+  "oh",
+  "End Of Road For GOTMILF License Plate - 'Offensive' vanity tag yanked by Washington officials. This is the story of GOTMILF.",
+  "the secret to comedy is that i was sexually abused 20 years ago. yeah, like a lot. no i don't recommend it",
+  "i'm in pain every day. this is a cry for help",
+  "COFFEE REVIEW: this fucked up roasted-seed tea pretends to be a bean when it's not even a legume. burnt, crushed to smithereens, and boiled away, it makes an astringent bitter brown brew that will leave you jittering and begging for more. 4/4, delicious",
+  "lyndon really do be johnsin'",
+  "santa claus is at this mall? what are the odds?",
+  "if bitcoin were any good there would be butchcoin. think about it",
+  "breathign a sigh of relief after Better Help told me hatsune miku isnt real. i could not f orgive her if she is real",
+  "if a centrist were to rank beverages would they say 'sodas aren't best hot or warm, they're best room temperature' or would they say 'the most flavorful beverage is water'",
+  "(planning a police kink scene) YOU'RE COMING WITH ME",
+  "the pain just does not stop",
+  "trust fund leftist. 'not in my back yard!!' he says. this is the thirteenth time this week i have broken in to use his parents trampoline",
+  "is my life forfeit",
+]
+target_post = raw_posts.shift();
+target_post_length = target_post.length;
+draft_post = "";
+genius_max = 1;
+
+last = performance.now();
+now = performance.now();
+delta = 0.125;
+
+
+function update(dt) {
+  // dt in ms; around 125
+  // console.log(dt); very close to 125
+  location.reload();
+}
+
+setInterval(
+  () => {
+    now = performance.now();
+    dt = (now - last);
+    last = now;
+    update(dt);
+  },
+  125); // about every 125ms
+
+function update(dt){
+  //console.log(dt);
+}
+
+function print_debug(header) {
+  console.log(header);
+  console.log(`
+  raw_posts: ${raw_posts.length} left;
+  target_post: ${target_post};
+  target_post_length: ${target_post_length};
+  draft_post: ${draft_post};
+  draft_post.length: ${draft_post.length};
+  `);
+  console.log(". . . . . . . . . . . . ");
+  console.log(`raw_posts: ${raw_posts}`);
+  console.log("========================");
+}
+
+function click_genius() {
+  var value = parseInt(document.getElementById('genius_level').value, 10);
+
+  if (value >= genius_max) {
+    // Try to type post
+    if (type_post()) {
+      // Post succesfully typed, reset genius level
+      value = 0
+      update_genius_max(Math.floor(genius_max * 1.001) + (genius_max <= 5 ? 1 : 0));
+    } else {
+      // Nah
+    }
+  } else {
+    // Increment value
+    value = (isNaN(value) ? 0 : value) + 1;
+  }
+
+  // Set value
+  document.getElementById('genius_level').value = value;
+
+  // Update CSS of genius bar
+  document.getElementById('genius_level_progress').value = value;
+  document.getElementById('genius_level_progress').max = genius_max;
+
+}
+
+function update_genius_max(new_max) {
+  genius_max = new_max;
+  document.getElementById('genius_level_progress').max = new_max;
+}
+
+function type_post() {
+  print_debug("type_post()");
+  if (target_post.length > 0) {
+    // Still have posts to make!
+    draft_post = draft_post + target_post.slice(0, 1);
+    target_post = target_post.slice(1); 
+
+    set_post_input(draft_post);
+
+    set_post_pie_percent(100 * (draft_post.length / target_post_length))
+
+    // Hack to expand postbox as needed
+    const ta = document.getElementById('post_input');
+    ta.style.height = ta.scrollHeight + 'px';
+    return true;
+  }
+  else {
+    // Can't type; post is ready to go
+    return false;
+  }
+}
+
+function set_post_input(value) {
+  document.getElementById('post_input').value = value;
+  if (draft_post.length == target_post_length) {
+    document.getElementById('post_send').disabled = false;
+  }
+}
+
+function post_send() {
+  if (draft_post.length == target_post_length) {
+    // Post the post!
+    create_post(draft_post);
+
+    // Reset everything
+    target_post = raw_posts.shift();
+    target_post_length = target_post.length;
+    draft_post = "";
+    set_post_pie_percent(0);
+    set_post_input("");
+    // Re-disable post
+    document.getElementById('post_send').disabled = true;
+    // Re-set height
+    const ta = document.getElementById('post_input');
+    ta.style.height = 'auto';
+    update_genius_max( Math.floor(genius_max * 1.05) + (genius_max <= 100 ? 1 : 0) );
+    
+  }
+
+  print_debug("post_send()");
+}
+
+function create_post(post_text) {
+  // Get the template
+  post_template = document.getElementById("post_template");
+  new_post = post_template.content.cloneNode(true);
+
+  // Get the container we want to put it in
+  post_feed = document.getElementById("post_feed");
+
+  // Put everything together
+  new_post.querySelector(".post_text").textContent = post_text;
+
+  post_feed.prepend(new_post);
+
+}
+
+
+function set_post_pie_percent(percent) {
+  post_pie = document.getElementById('post_pie');
+  degree = percent * 360.0 / 100 + 2;
+  color = (percent >= 100.0) ? "#2d4" : "#38d";
+  othercolor = (percent >= 100.0) ? "#2d4" : "#0000";
+  post_pie.style.backgroundImage =
+    `conic-gradient(${color} ${degree}deg, ${othercolor} ${degree}deg 360deg)`;
+}
+
+set_post_pie_percent(0);
